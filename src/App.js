1b60d9889979fcc53848abcfd95ab0c2a701b419
Swoyen <Swoyen>
2021-06-01 20:25:32 +0930

    updated readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="md">
+      <Typography variant="h2" color="initial">
+        Hello
+      </Typography>
+    </Container>
   );
-}
+};
 
 export default App;
