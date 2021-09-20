a7d4f934b2034cbf2feb217f62ad85eb1dab7969
Swoyen <Swoyen>
2021-09-20 16:20:34 +0930

    changed basee_url to azure webapi

--- src/api/config.js
+++ src/api/config.js
@@ -3,8 +3,10 @@
 // export const BASE_URL = "https://localhost:5001/api/";
 // export const BASE_URL = "http://localhost:8080/api/";
 
-export const BASE_URL =
-  "http://bugtrackerapicontainer.australiaeast.azurecontainer.io/api/";
+// export const BASE_URL =
+//   "http://bugtrackerapicontainer.australiaeast.azurecontainer.io/api/";
+
+export const BASE_URL = "https://buhtrackerapi.azurewebsites.net/api/";
 export const IMAGE_URL =
   "https://bugtrackerapi-bucket.s3.ap-southeast-2.amazonaws.com/attachments/";
 //export const SUBSCRIPTION_KEY = "e8c7362cd47b4332a37b874bb9448c95";
